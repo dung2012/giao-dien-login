c9d0097ca90eb44d53b83c74fb1817fded332ec9
dung2012 <dungchuhung@gmail.com>
2024-01-25 20:18:10 +0700

    demo02

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="login-background">
+      <div className="login-container">
+        <div className="login-content row">
+          <div className="col-12 text-login">WELCOME</div>
+          <div className="col-12 form-group login-input">
+              <div>Username</div>
+              {/* <i class="fa-solid fa-user"></i> */}
+              <input type = "text" className="form-control" placeholder="Enter your usename" />
+          </div>
+          <div className="col-12 form-group login-input">
+              <div>Password</div>
+              <input type = "text" className="form-control" placeholder="Enter your password" />
+          </div>
+          <div className="col-12 forgot-password">
+              <label className="forgot-password">Forgot Password?</label>
+          </div>
+          <div className="col-12">
+              <button className="btn-login">LOGIN</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
